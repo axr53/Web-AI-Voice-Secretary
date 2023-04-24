8e1b4af0bea35b8ade6eb29847fc2c305f138cfd
unknown <gentlemat432@gmail.com>
2023-04-24 10:40:57 +0900

    notebook sixver

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    
+    // 로그인 처리
+    // ...
+  }
+  
